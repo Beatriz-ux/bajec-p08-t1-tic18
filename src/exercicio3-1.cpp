630aeed4f3ea3b6b92383a1e1f1448882853dbed
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-06 12:07:56 -0300

    feat: exercicio 3-1c

--- src/exercicio3-1.cpp
+++ src/exercicio3-1.cpp
@@ -130,6 +130,49 @@ int main(){
         cout << "Data invalida" << endl;
     }
 
+    // c)
+    cout << "\nData por extenso :" << dia << " de ";
+    switch (mes){
+            case 1:
+                cout << "Janeiro";
+                break;
+            case 2:
+                cout << "Fevereiro";
+                break;
+            case 3:
+                cout << "Março";
+                break;
+            case 4:
+                cout << "Abril";
+                break;
+            case 5:
+                cout << "Maio";
+                break;
+            case 6:
+                cout << "Junho";
+                break;
+            case 7:
+                cout << "Julho";
+                break;
+            case 8:
+                cout << "Agosto";
+                break;
+            case 9:
+                cout << "Setembro";
+                break;
+            case 10:
+                cout << "Outubro";
+                break;
+            case 11:
+                cout << "Novembro";
+                break;
+            case 12:
+                cout << "Dezembro";
+                break;
+        }
+        cout << " de " << ano << endl;
+    
+
 
     return 0;
 }
