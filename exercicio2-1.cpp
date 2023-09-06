c59bec14c4cdbfd38cc34398d1926dc31a702835
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-06 09:14:52 -0300

    feat: exercicio 2-1b 2-1c

--- exercicio2-1.cpp
+++ exercicio2-1.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
-    int i;
-    int cont[20] = {0}, num[100];
+    int i, j, max = 0;
+    int cont[20] = {0}, num[100], numMax[20] = {0};
+    srand(time(NULL));
 
     //a)
+    cout << "Numeros aleatorios: \n";
     for(i=0; i<100; i++){
         num[i] = 1 + rand()%20;
+        cout << i+1 << "\n" << num[i] << endl;
+
+        //b)
+        cont[num[i]-1]++;
     }
+
+    cout << "\nQuantidade de repetições: \n";
+    for (i=0; i<20; i++){
+        cout << i+1 << "\t" << cont[i] <<" | " ;
+        for (int j=0; j<cont[i]; j++){
+            cout << "#";
+        }
+        cout << endl;
+    }
+
+    //c)
+    for (i=0; i<20; i++){
+        if (cont[i] > max){
+            max = cont[i];
+        }
+    }
+
+    for (i=0, j=0; i<20; i++){
+        if (cont[i] == max){
+            numMax[j] = i+1;
+            j++;
+        }
+    }
+
+    cout << "\nO(s) numero(s) que mais aparece(m) e(sao): ";
+    i = 0;
+    while(numMax[i] !=0){
+        cout << numMax[i] << " ";
+        i++;
+    }
+    cout << endl;
+
+
+
+
+    cout << "";
+
     return 0;
 }
