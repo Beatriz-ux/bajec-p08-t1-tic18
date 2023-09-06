a04446d3c50280a65a2e9b136ae1c0da723546fc
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-06 10:06:28 -0300

    feat: exercicio 2-3b, exercicio 2-3c,

--- exercicio2-3.cpp
+++ exercicio2-3.cpp
@@ -3,13 +3,23 @@
 using namespace std; 
 
 int main(){
-    float notas[15];
+    float notas1[15], notas2[15];
     srand(time(NULL));
 
-    // a)
-    for (int i=0; i<15; i++)
-        notas[i] = ((float)rand()/RAND_MAX)*10;
+    for (int i=0; i<15; i++){
+        // a)
+        notas1[i] = ((float)rand()/RAND_MAX)*10;
 
-    
+        // b)
+        notas2[i] = ((float)rand()/RAND_MAX)*10;
+
+        // c)
+        cout << "Aluno " << i+1 << ":" ;
+        cout <<((notas1[i] == notas2[i]) ? " Manteve a nota" : 
+                ((notas1[i] > notas2[i])? " Piorou": " Melhorou")) << endl;
+        cout << "Nota 1 = " << notas1[i] << " | Nota 2 = " << notas2[i]<< "\n\n";
+    }
+
+
     return 0;
 }
