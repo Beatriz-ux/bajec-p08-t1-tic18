f716f452c9771cca47598496ddbd260b02557011
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-09-06 10:10:16 -0300

    feat: add exercicio 2-2

---
+++ src/exercicio2-2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+#define TAM 250
+
+int main(void){
+    srand(time(NULL));
+
+    double temperaturas[TAM];
+    double max = 0.0, min = 50.0;
+    double media = 0.0;
+
+    for(int i = 0; i < TAM; i++){
+        temperaturas[i] = 10 + 30.0 * (1.0*rand()/RAND_MAX);
+
+        if (i < 10 || (i >= (TAM/2)-5 && i < (TAM/2)+5) || i >= TAM - 10)
+            cout << "Temperatura " << i+1 << ": " << temperaturas[i] << endl;
+
+        media += temperaturas[i];
+
+        if(temperaturas[i] > max){
+            max = temperaturas[i];
+        }
+        else if(temperaturas[i] < min){
+            min = temperaturas[i];
+        }
+    }
+
+    media /= TAM;
+
+    cout << endl;
+    cout << "#################################" << endl;
+    cout << "Temperatura maxima: " << max << endl;
+    cout << "Temperatura minima: " << min << endl;
+    cout << "Temperatura media: " << media << endl;
+    cout << "#################################" << endl;
+    cout << endl;
+
+    for(int i = 0; i < TAM; i++){
+        if(temperaturas[i] > media){
+            temperaturas[i] += 1;
+        }
+        else if(temperaturas[i] < media){
+            temperaturas[i] -= 2;
+        }
+
+        if (i < 10 || (i >= (TAM/2)-5 && i < (TAM/2)+5) || i >= TAM - 10)
+            cout << "Temperatura " << i+1 << ": " << temperaturas[i] << endl;
+    }
+
+    return 0;
+}
