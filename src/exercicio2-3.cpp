8e4684bceb46b8ae20afc395be6314559f496c0a
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-09-07 12:19:43 -0300

    fix: melhorando o desempenho do programa. fix: deixando a visualização das informações mais organizada no console

--- src/exercicio2-3.cpp
+++ src/exercicio2-3.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 
 using namespace std;
-int main(void){
-    float notas[15]={0}, notas2[15]={0} ,media[15]={0};
+int main(void)
+{
+    float notas[15] = {0}, notas2[15] = {0}, media[15] = {0};
     cout << fixed;
     cout.precision(2);
-    for(int i=0; i<15;i++){
-        notas[i] = ((float)rand()/RAND_MAX)*10;
-        cout << "Nota1 do aluno["<<i+1<<"]" << ": "<< notas[i] << endl;
+    for (int i = 0; i < 15; i++)
+    {
+        notas[i] = ((float)rand() / RAND_MAX) * 10;
+        notas2[i] = ((float)rand() / RAND_MAX) * 10;
+        media[i] = (notas[i] + notas2[i]) / 2;
     }
-    cout << endl;
-    for(int i=0; i<15;i++){
-        notas2[i] = ((float)rand()/RAND_MAX)*10;
-        cout << "Nota2 do aluno ["<<i+1<<"]" << ": "<< notas2[i] << endl;
-    }
-    for(int i=0; i<15;i++){
-        if (notas[i] > notas2[i]){
-            cout << "Aluno ["<<i+1<<"]" << " piorou "<< endl;
+    cout << "\t\tDESEMPENHO DOS ALUNOS" << endl;
+    cout << "--\t"
+         << "n1\t"
+         << "n2\t"
+         << "desempenho\t"
+         << "media\t" << endl;
+    for (int i = 0; i < 15; i++)
+    {
+        cout << "[" << i + 1 << "]"
+             << "\t" << notas[i] << "\t" << notas2[i] << "\t";
+        if (notas[i] > notas2[i])
+        {
+            cout << "piorou  ";
         }
-        else if (notas[i] < notas2[i]){
-            cout << "Aluno ["<<i+1<<"]" << " melhorou "<< endl;
+        else if (notas[i] < notas2[i])
+        {
+            cout << "melhorou ";
         }
-        else{
-            cout << "Aluno ["<<i+1<<"]" << " manteve a mesma nota"<< endl;
+        else
+        {
+            cout << "manteve ";
         }
-    }
-
-    for(int i=0; i<15;i++){
-        media[i] = (notas[i] + notas2[i])/2;
-        cout << "Media do aluno ["<<i+1<<"]" << ": "<< media[i] << endl;
+        cout << "\t" << media[i] << endl;
     }
     return 0;
 }
