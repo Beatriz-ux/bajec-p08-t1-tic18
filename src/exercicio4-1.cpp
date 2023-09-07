39f1c7ee0cbb6bc9bb9c04b9ee371ff19101074a
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-09-07 16:16:34 -0300

    fix: eliminando cout, para diminuir o tempo de resposta fix: diminuindo a escala do histograma

--- src/exercicio4-1.cpp
+++ src/exercicio4-1.cpp
@@ -7,15 +7,16 @@ int main (void){
     for (int i = 0; i < 640; i++){
         for (int j = 0; j < 480; j++){
             img[i][j] = rand() % 256;
-            cout << img[i][j] << endl;
+            //cout << img[i][j] << endl; //para visualizar as cores que estão sendo geradas
             tom[img[i][j]]++;
         }
     }
+    //è possivel que uma cor possa ter ate 307200 de frequencia, então para ser visualizado o histograma, foi dividido por 10
     cout <<"\t Histograma: " << endl;
     cout << "Tom\t Frequencia" << endl;
     for (int i = 0; i < 256; i++){
         cout << i << "\t ";
-        for (int j = 0; j < tom[i]; j++){
+        for (int j = 0; j < (tom[i]/10); j++){
             cout << "*";
 
         }
