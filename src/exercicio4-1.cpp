747b6e05ab77089d1903063e6ef95aa7a0f6ae2c
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-07 22:18:28 -0300

    feat: exercicio 4-1b

--- src/exercicio4-1.cpp
+++ src/exercicio4-1.cpp
@@ -4,14 +4,29 @@
 using namespace std;
 
 int main(){
-    int h=640, w=480, i, j;
-    char imagem[h][w];
+    int h=10, w=10, i, j;
+    short int imagem[h][w]; 
+    int histograma[256]{0};
     srand(time(NULL));
 
     // a)
     for(i=0; i<h; i++){
         for(j=0; j<w; j++)
             imagem[i][j] = rand()%256;
+    }
+
+    //b)
+    for(i=0; i<h; i++){
+        for(j=0; j<w; j++)
+            histograma[(int)imagem[i][j]]++;       
+    }
+
+    // print histograma
+    for(i=0; i<256; i++){
+        cout << i << " ";
+        for(j=0; j<histograma[i]; j++)
+            cout << "*";
+        cout << " " << histograma[i] << endl;
     }
 
 
