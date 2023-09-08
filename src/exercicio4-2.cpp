5f3331e52a996e675cf12e4de283bb73637ec141
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-07 22:27:59 -0300

    feat: exercicio4-2a

---
+++ src/exercicio4-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    int qntdStrings = 10, tamString = 10; 
+    char strings[qntdStrings][tamString+1]; 
+    int i, j;
+    srand(time(NULL));
+
+    // a)
+    for(i=0; i<qntdStrings; i++){
+        for(j=0; j<=tamString; j++){
+            if(j == tamString)
+                strings[i][j] = '\0';
+            else{
+                strings[i][j] = 'a' + (rand()%('z' - 'a'));
+                strings[i][j] = 'a' + (rand()%('z' - 'a'));
+            }
+        }
+    }
+
+    return 0;
+}
